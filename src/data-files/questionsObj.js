bdf2e228aabdb8963918ea5bbc18ba661f98f78a
annasshah <anasshahskype@gmail.com>
2021-10-29 17:29:36 +0500

    React Assignment # 04

---
+++ src/data-files/questionsObj.js
@@ -0,0 +1,134 @@
+
+
+
+
+
+
+let questions = [
+    {
+        "answer": "Namibia",
+        "options": [
+            "Namibia",
+            "Niger",
+            "Chad",
+            "Ghana"
+        ],
+        "question": "Windhoek is the capital of__________?",
+        "userAnswer":'nil',
+        "score":0
+    },
+    {
+        "answer": "Euro",
+        "options": [
+            "Zolty",
+            "Peso",
+            "Euro",
+            "Escudo"
+        ],
+        "question": "The currency of portugal is__________?",
+        "userAnswer":'nil',
+        "score":0
+    },
+    {
+        "answer": "Almaty",
+        "options": [
+            "Astana",
+            "Almaty",
+            "Baku",
+            "Tashkent"
+        ],
+        "question": "Which of the following city served as capital of Kazakhstan till 1997?",
+        "userAnswer":'nil',
+        "score":0
+    },
+    {
+        "answer": "North Atlantic Ocean",
+        "options": [
+            "North Atlantic Ocean",
+            "South Atlantic Ocean",
+            "Pacific Ocean",
+            "Arabian Sea"
+        ],
+        "question": "Bermuda Triangle is located in__________?",
+        "userAnswer":'nil',
+        "score":0
+    },
+    {
+        "answer": "Ramon Magsaysay Award",
+        "options": [
+            "Ramon Magsaysay Award",
+            "Booker Prize",
+            "Pulitzer Prize",
+            "None of them"
+        ],
+        "question": "Which among the following is known as Asia a version of “Noble prize”?",
+        "userAnswer":'nil',
+        "score":0
+    },
+    {
+        "answer": "Geneva",
+        "options": [
+            "London",
+            "Geneva",
+            "Rome",
+            "Paris"
+        ],
+        "question": "The headquarter of EFTA is in_____________?",
+        "userAnswer":'nil',
+        "score":0
+    },
+    {
+        "answer": "China",
+        "options": [
+            "Japan",
+            "China",
+            "South Koera",
+            "None of These"
+        ],
+        "question": "Which country Built A 250-Acre Solar Farm Shaped Like A Giant Panda:",
+        "userAnswer":'nil',
+        "score":0
+    },
+    {
+        "answer": "White Bellbird",
+        "options": [
+            "Macaws",
+            "Cockatoos",
+            "White Bellbird",
+            "None of these"
+        ],
+        "question": "________ is the World’s Loudest Bird",
+        "userAnswer":'nil',
+        "score":0
+
+    },
+    {
+        "answer": "Feb 29",
+        "options": [
+            "Feb 29",
+            "Feb 28",
+            "March 01",
+            "Feb 01"
+        ],
+        "question": "_______ is known as leap day or leap year day",
+        "userAnswer":'nil',
+        "score":0
+
+    },
+    {
+        "answer": "China",
+        "options": [
+            "Japan",
+            "Russia",
+            "China",
+            "None of These"
+        ],
+        "question": "Where is the World’s longest sea bridge ?",
+        "userAnswer":'nil',
+        "score":0
+
+    }
+]
+
+
+export default questions 
